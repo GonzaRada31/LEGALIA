5298a9a886b4b1c4c299c85186641f8f3ad48b6f
GonzaloRada <33236294@fi.unju.edu.ar>
2024-04-19 14:34:05 -0300

    Actualizacion de urls del proyecto

--- projectlegal/urls.py
+++ projectlegal/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for projectlegal project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from AppLegal.views import home_view    # Importamos la vista home_view.
+#from AppLegal.views import GenerateTextView, DocumentSummaryView, TextClassificationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('AppLegal.urlsapp')),  # Organiza bajo el prefijo 'api/',
+    path('', home_view, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
